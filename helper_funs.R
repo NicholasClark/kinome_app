73397afb2a2c1591cff227615ee258bff92a0106
Nicholas Clark <nicholas.clark00@gmail.com>
2023-01-23 23:08:07 -0500

    adding helper functions

---
+++ helper_funs.R
@@ -0,0 +1,17 @@
+#### Helper functions
+
+
+### Get synapse credentials
+get_synapse_credentials = function() {
+  readLines("synapse_auth")
+}
+
+### Download synapse folder
+download_synapse_data = function() {
+  syn_cred = get_synapse_credentials()
+  cmd1 = paste0("synapse -p ", syn_cred, " get -r syn50909160", sep = "")
+  system("pip install synapseclient")
+  system(cmd1)
+}
+
+
